a2b739a9552e64179d0846654a67a529df2193bd
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2020-12-06 20:50:54 +0100

    add mock script

---
+++ server/src/mock.ts
@@ -0,0 +1,19 @@
+import { loremIpsum } from 'lorem-ipsum';
+import { Note, NoteBuilder } from './models/entities/note';
+import { InMemoryDatabase } from './utils/in-memory-database';
+
+(async function main() {
+  const db = new InMemoryDatabase();
+  const noteRepo = db.getRepository(Note);
+  for (let i = 0; i < 1000; i++) {
+    await noteRepo.save(
+      new NoteBuilder({
+        content: loremIpsum({
+          count: 2,
+          sentenceLowerBound: 10,
+          sentenceUpperBound: 100,
+        }),
+      }).build()
+    );
+  }
+})();
